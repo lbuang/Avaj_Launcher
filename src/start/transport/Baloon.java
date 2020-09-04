959951797bead2a811c7dfded27446355dd48e89
Lucky Buang <luckyb@bbd.co.za>
2020-09-04 08:43:57 +0200

    update

--- src/start/transport/Baloon.java
+++ src/start/transport/Baloon.java
@@ -19,23 +19,23 @@ public class Baloon extends Aircraft implements Flyable {
                 this.coordinates.setHeight(this.coordinates.getHeight() + 4);
                 if (this.coordinates.getHeight() > 100)
                     this.coordinates.setLongitude(100);
-                WriteToTXT.getWriteToTXT().writeTXT("Baloon#" + this.name + "(" + this.id + "): GOOD THING I BROUGHT MY SUNGLASSES");
+                WriteToTXT.getWriteToTXT().writeTXT("Baloon#" + this.name + "(" + this.id + "): KEEP YOUR FACE TO THE SUN AND YOU WILL NEVER SEE THE SHADOWS");
                 break;
             case "RAIN":
                 this.coordinates.setHeight(this.coordinates.getHeight() - 5);
-                WriteToTXT.getWriteToTXT().writeTXT("Baloon#" + this.name + "(" + this.id + "): I WISH THIS BALLON WASN'T OPEN");
+                WriteToTXT.getWriteToTXT().writeTXT("Baloon#" + this.name + "(" + this.id + "): RAIN FALLS AS GENTLY AS MERCY.\n");
                 break;
             case "FOG":
                 this.coordinates.setHeight(this.coordinates.getHeight() - 3);
-                WriteToTXT.getWriteToTXT().writeTXT("Baloon#" + this.name + "(" + this.id + "): I CAN'T SEE!!!");
+                WriteToTXT.getWriteToTXT().writeTXT("Baloon#" + this.name + "(" + this.id + "): FOG IS THE MYSTERY ITSELF!");
                 break;
             case "SNOW":
                 this.coordinates.setHeight(this.coordinates.getHeight() - 15);
-                WriteToTXT.getWriteToTXT().writeTXT("Baloon#" + this.name + "(" + this.id + "): IM STARTING TO FREEZE UP HERE!");
-                WriteToTXT.getWriteToTXT().writeTXT("Tower  says: Baloon#" + this.name + "(" + this.id + ")" + " unregistered from weather tower.");
+                WriteToTXT.getWriteToTXT().writeTXT("Baloon#" + this.name + "(" + this.id + "): IT'S SNOWING TOO BAD YOU GOT A BALOON :)");
+                WriteToTXT.getWriteToTXT().writeTXT("Tower  says: Baloon#" + this.name + "(" + this.id + ")" + " UNREGISTERED FROM THE WEATHER TOWER.");
                 break;
             default:
-                WriteToTXT.getWriteToTXT().writeTXT("Baloon#" + this.name + "(" + this.id + "): I CANT CONTACT THE WEATHER TOWER");
+                WriteToTXT.getWriteToTXT().writeTXT("Baloon#" + this.name + "(" + this.id + "): I CANT CONNECT THE WEATHER TOWER");
                 break;
         }
         if (this.coordinates.getHeight() <= 0)
@@ -45,7 +45,7 @@ public class Baloon extends Aircraft implements Flyable {
     }
     public	void	registerTower(WeatherTower weatherTower) {
         this.weatherTower = weatherTower;
-        WriteToTXT.getWriteToTXT().writeTXT("Tower says: Baloon#" + this.name + "(" + this.id + ")" + " registered to weather tower.");
+        WriteToTXT.getWriteToTXT().writeTXT("Tower says: Baloon#" + this.name + "(" + this.id + ")" + " REGISTERED TO THE WEATHER TOWER.");
         weatherTower.register(this);
     }
 }
